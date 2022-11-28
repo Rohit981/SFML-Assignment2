26160e2c650e3d8714e92bfbdd85096484f618f5
Rohit981 <46937934+Rohit981@users.noreply.github.com>
2022-11-28 01:03:23 -0800

    Final Commit

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <fstream>
+
 
 
 //Namespace
@@ -21,12 +23,14 @@ const int WORLD_SIZE = 10;
 int coordinateWorld[WORLD_SIZE][WORLD_SIZE];
 Sprite SpritePosition[WORLD_SIZE][WORLD_SIZE];
 // loading info
-int browsingIndex = 0;
 Sprite tileSprite[200];
 int TextureValue = 0;
 int CurrentTextureValue = 0;
 int CurrentSpriteValue = 0;
 bool isLeftMouseClicked = false;
+bool isLevelLoaded = false;
+bool isLevelSaved = false;
+
 
 
 
@@ -37,8 +41,8 @@ void Update(RenderWindow& window);
 void Render(RenderWindow& window);
 void ChangeTile(int ComparisonValue, int Value);
 void OnLeftMouseClick(RenderWindow& window);
-
-
+bool LoadLevel();
+bool SaveLevel();
 
 
 
@@ -127,6 +131,11 @@ void handleInput(RenderWindow& window, Event e)
 
         CurrentSpriteValue += 1;
 
+        int spriteXIndex = Mouse::getPosition(window).x / 70;
+        int spriteYIndex = Mouse::getPosition(window).y / 70;
+
+        coordinateWorld[spriteXIndex][spriteYIndex] = CurrentSpriteValue;
+
         //cout << "Tile X Position:" << Mouse::getPosition(window).x - ((Mouse::getPosition(window).x % 70)) << endl;
 
     }
@@ -138,6 +147,20 @@ void handleInput(RenderWindow& window, Event e)
         if (texture.copyToImage().saveToFile("Homework 2 Screenshot.png"))
         {
             std::cout << "Screenshot saved to Homework 2 Screenshot.png" << std::endl;
+        }
+    }
+
+    else if (Keyboard::isKeyPressed(Keyboard::L))
+    {
+        isLevelLoaded = LoadLevel();
+            if (!isLevelLoaded) {
+                cout << "ERROR: failed to load. Level.txt does not exist." << endl;
+            }
+    }else if (Keyboard::isKeyPressed(Keyboard::S)) 
+    {
+         isLevelSaved = SaveLevel();
+        if (!isLevelSaved) {
+            cout << "failed to save" << endl;
         }
     }
 }
@@ -202,7 +225,92 @@ void OnLeftMouseClick(RenderWindow& window)
     {
         tileSprite[CurrentSpriteValue].setTexture(tileTexture[TextureValue]);
         tileSprite[CurrentSpriteValue].setPosition(Mouse::getPosition(window).x, Mouse::getPosition(window).y);
+        
 
     }
 
+}
+
+//Loading of Level
+bool LoadLevel()
+{
+    const char* fileName = "level.txt";
+    ifstream inFile(fileName);
+    if (inFile.good()) 
+    {
+        int data[WORLD_SIZE * WORLD_SIZE] = { 0 };
+        int number = 0;
+
+        int index = 0;
+        for (int i = 0; i < WORLD_SIZE * WORLD_SIZE; i++) 
+        {
+            inFile >> number;
+            cout << number << ", ";
+            data[index] = number;
+
+            index++;
+        }
+
+        index = 0;
+        for (int i = 0; i < WORLD_SIZE; i++) 
+        {
+            for (int j = 0; j < WORLD_SIZE; j++) 
+            {
+                int textureValue = data[index];
+
+                cout << "loading data:" << textureValue << " | index:" << index << endl;
+                coordinateWorld[i][j] = textureValue;
+
+                if (textureValue != -1) 
+                {
+                    Sprite dataSprite = Sprite();
+                    dataSprite.setTexture(tileTexture[textureValue]);
+                    dataSprite.setPosition(i * 70.f, j * 70.f);
+
+                    tileSprite[i] = dataSprite;
+                }
+
+                index++;
+            }
+        }
+        cout << endl;
+        inFile.close();
+        isLevelLoaded = true;
+    }
+
+    return isLevelLoaded;
+}
+
+//Saving the level
+bool SaveLevel()
+{
+    int levelData[WORLD_SIZE * WORLD_SIZE];
+    int indexer = 0;
+    for (int i = 0; i < WORLD_SIZE; i++) 
+    {
+        for (int j = 0; j < WORLD_SIZE; j++) 
+        {
+            levelData[indexer] = coordinateWorld[i][j];
+            indexer++;
+            cout << "writing char:" << indexer << endl;
+        }
+    }
+
+    const int size = WORLD_SIZE * WORLD_SIZE;
+    ofstream myfile("level.txt");
+    if (myfile.is_open())
+    {
+        for (int count = 0; count < size; count++) 
+        {
+            myfile << levelData[count] << " ";
+        }
+        myfile.close();
+        isLevelSaved = true;
+    }
+    else 
+    {
+        cout << "Unable to open file";
+    }
+    return isLevelSaved;
+    return false;
 }
